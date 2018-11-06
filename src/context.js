458149a0abc3ed96d69f6360375ceb2e9a6ca0da
shitashikat <radistkakat89@gmail.com>
2018-11-06 11:12:08 +0200

    committtt

---
+++ src/context.js
@@ -0,0 +1,58 @@
+//0cd6e37f6c88144998b992963c0e5d30
+//http://api.musixmatch.com/ws/1.1/
+//chart.tracks.get?chart_name=top&page=1&page_size=5&country=it&f_has_lyrics=1
+//&apikey=${process.env.REACT_APP_MM_KEY}
+
+import React, { Component } from "react";
+import axios from "axios";
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_TRACKS":
+      return {
+        ...state,
+        track_list: action.payload,
+        heading: "Search Results"
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    track_list: [],
+    heading: "Top 10 Tracks",
+    dispatch: action => this.setState(state => reducer(state, action))
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+          process.env.REACT_APP_MM_KEY
+        }`
+      )
+      .then(res => {
+        /* console.log(process.env.REACT_APP_MM_KEY);
+        console.log(res.data); */
+        this.setState({ track_list: res.data.message.body.track_list });
+      })
+      .catch(err => {
+        console.log(process.env.REACT_APP_MM_KEY);
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
